dd99c644309726314eb328f9267ee4935bacc4e7
ArifinPaiton <m.5y4msulpaiton@gmail.com>
2025-05-09 21:22:58 +0700

    menambah file buffer.mjs

---
+++ buffer.mjs
@@ -0,0 +1,7 @@
+const buffer = Buffer.from("M Syamsul Arifin");
+
+console.info(buffer); // menampilkan buffer berupa byte
+console.info(buffer.toString()); // mengubah dari byte ke string
+
+buffer.reverse(); // membalikkan urutan byte
+console.info(buffer.toString()); // menampilkan buffer yang sudah dibalik dalam bentuk string
