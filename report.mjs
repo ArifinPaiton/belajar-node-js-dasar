675d6606634ed888bcf32938ae6a9e363a9fdb6e
ArifinPaiton <m.5y4msulpaiton@gmail.com>
2025-05-09 21:06:32 +0700

    menambah komentar

--- report.mjs
+++ report.mjs
@@ -3,6 +3,7 @@ import process from "process";
 process.report.reportOnFatalError = true;
 process.report.reportOnSignal = true;
 process.report.reportOnUncaughtException = true;
+// hasil dari reportnya di simpan ke dalam file report.json
 process.report.filename = "report.json";
 
 function sampleError() {
